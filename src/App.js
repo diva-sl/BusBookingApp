e8850edd6ae2d55fa459a32cd99fd2b4d15c626c
Divakaran S <divakaran.sivasamy@gmail.com>
2024-06-24 10:30:01 +0530

    app.js files and fuctions are declare and created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect } from "react";
+import Login from ".pages/Login";
+import Register from ".pages/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PublicRoute from "./component/PublicRoute";
+import ProtecedRoute from "./component/ProtecedRoute";
+import Home from "./pages/Home";
+import AdminBuses from "./pages/admin/AdminBuses";
+import AdminHome from "./pages/admin/AdminHome";
+import AdminUsers from "./pages/admin/AdminUsers";
+import BookNow from "./pages/BookNow";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtecedRoute>
+                <Home />
+              </ProtecedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtecedRoute>
+                <AdminHome />
+              </ProtecedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtecedRoute>
+                <AdminBuses />
+              </ProtecedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtecedRoute>
+                <AdminUsers />
+              </ProtecedRoute>
+            }
+          />
+          <Route
+            path="/book-now/:id"
+            element={
+              <ProtecedRoute>
+                <BookNow />
+              </ProtecedRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
