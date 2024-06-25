2dd800b417be7cd2773d658167a44e981a9e5bc0
Divakaran S <divakaran.sivasamy@gmail.com>
2024-06-25 16:39:31 +0530

    Loader file created and function declared.

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../pages/global.css";
+import { RiLoader5Line } from "react-icons/Ri";
+
+function Loader() {
+  return (
+    <div className="spinner">
+      <RiLoader5Line size={"7vw"} classsName="App-logo" />
+    </div>
+  );
+}
+export default Loader;
