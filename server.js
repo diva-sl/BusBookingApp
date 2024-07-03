c529731f4858bcacbdf1b134bcf48790a5c00029
Divakaran S <divakaran.sivasamy@gmail.com>
2024-07-03 11:13:47 +0530

    env file for connection to database postgres

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const userRoute = require("./routes/userRoute.js");
+const busRoute = require("./routes/busRoute.js");
+const bookingRoute = require("./routes/bookingRoute.js");
+require("dotenv").config();
+const dbConnect = require("./config/connect.js");
+const app = express();
+
+app.use(cors());
+
+app.use(express.join());
+app.use("/user", userRoute);
+app.use("/buses", busRoute);
+app.use("/api/booking", bookingRoute);
+
+app.listen(5000, () => {
+  console.log("in");
+});
