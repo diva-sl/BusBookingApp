8c40f88d4ee722c52ad0989efe3cd35eb73e7cb5
Divakaran S <divakaran.sivasamy@gmail.com>
2024-07-03 11:01:20 +0530

    User route file created and coded.

---
+++ routes/userRoute.js
@@ -0,0 +1,97 @@
+const router = require("express0").Router();
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authMiddleare = require("../middleware/authMiddleware.js");
+
+router.post("/register", async (req, res, next) => {
+  const { name, email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(500).send({
+        message: "User Already Exists",
+        success: false,
+        data: null,
+      });
+    }
+    const hashedPassword = await bcrypt.hashSync(password);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+    if (user) {
+      return res.send({
+        message: "User Created Successfully",
+        success: true,
+        data: null,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.send({
+        message: "User Not Found",
+        success: false,
+        data: null,
+      });
+    }
+    const isCorrectPassword = await bcrypt.compareSync(
+      password,
+      existingUser.password
+    );
+    if (!isCorrectPassword) {
+      return res.send({
+        message: "User Not Found",
+        success: false,
+        data: null,
+      });
+    }
+    const token = jwt.sign({
+      id: existingUser._id,
+      token: {
+        expiresIn: "id",
+      },
+    });
+    res.send({
+      message: "User Logged In Successfully",
+      seccess: true,
+      data: token,
+    });
+  } catch (error) {
+    res.send({
+      message: error,
+      success: false,
+      data: null,
+    });
+  }
+});
+
+router.post("/getuser", authMiddleare, async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId);
+    res.send({
+      message: "User Fetched Successfully",
+      data: user,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(401).send({
+      message: "Authentication Failed.",
+      success: false,
+    });
+  }
+});
+
+module.exports = router;
