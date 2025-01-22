fa7d4c382cdfd52351a32298250b6f41deaa2c2d
diva-sl <divakaran.sivasamy@gmail.com>
2025-01-22 13:22:51 +0530

    added node version ...

--- api/server.js
+++ api/server.js
@@ -1,14 +1,11 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const userRoute = require("./users.js");
 const busRoute = require("./buses.js");
 const bookingRoute = require("./booking.js");
-const connectDB = require("./config/connect.js");
 
 const app = express();
-// Connect to the database
-//connectDB();
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -19,22 +16,4 @@ app.use("/users", userRoute);
 app.use("/buses", busRoute);
 app.use("/api/bookings", bookingRoute);
 
-// heroku enviroment
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   // const path = require("path");
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-//   });
-// }
-// Start server
-app.use(
-  cors({
-    origin: "https://vought-bus.vercel.app", // Replace with your frontend URL
-    credentials: true,
-  })
-);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
