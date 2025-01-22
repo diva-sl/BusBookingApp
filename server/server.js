fb4e618425d1768a1bdb079c46b6e54bdf136d13
diva-sl <divakaran.sivasamy@gmail.com>
2025-01-22 18:27:48 +0530

    database connection build properly and updated.

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use(
 );
 
 // Start server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
